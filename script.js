53fa319c57cb91111b99e2b67545066f5ef45fac
Laubersheini <d.laubersheimer@t-online.de>
2021-04-24 22:57:32 +0200

    initial comit

---
+++ script.js
@@ -0,0 +1,123 @@
+var canvas = document.getElementById("canvas")
+var ctx = canvas.getContext("2d");
+
+
+
+class Particle{
+
+	constructor({
+		x=0,
+		y=0,
+		dx = 0,
+		dy = 0,
+		ax = 0,
+		ay = 0,
+		width = 10,
+		height = 10,
+		color = "#ff00ff"
+	}){
+			
+		this.x = x;
+		this.y = y;
+		this.dx = dx;
+		this.dy = dy;
+		this.ax= ax;
+		this.ay = ay;
+		this.width = width
+		this.height= height
+		this.color = color;
+	}
+	
+	update(){
+		this.x += this.dx	
+		this.y += this.dy
+		this.dx += this.ax
+		this.dy += this.ay
+		this.draw();	
+	}	
+
+	draw(){
+		ctx.fillStyle = this.color;
+		ctx.fillRect(this.x,this.y,this.width,this.height);
+		
+
+	}
+
+}
+
+
+var particles = [];
+
+for(let i=0;i< 200;i++){
+	particles[i] = generateRandomParticle({
+	x:500,
+	y:500,
+	dy:1,
+	ay:0.1
+	},{
+	spreadDx :5, 
+	spreadDy :0.5
+
+	});
+}
+
+
+function generateRandomParticle(
+	{
+		x=0,
+		y=0,
+		dx = 0,
+		dy = 0,
+		ax = 0,
+		ay = 0,
+		width = 10,
+		height= 10	
+},{	
+	spreadX = 0,
+		spreadY = 0,
+		spreadDx = 0,
+		spreadDy = 0,
+		spreadAx = 0,
+		spreadAy = 0,
+		spreadWidth =0,
+		spreadHeight = 0
+
+	}){
+	
+	x += generateSpread(spreadX)
+	y += generateSpread(spreadY)
+	dx += generateSpread(spreadDx)
+	dy += generateSpread(spreadDy)
+	ax += generateSpread(spreadAx)
+	ay += generateSpread(spreadAy)
+	width += generateSpread(spreadWidth)
+	height  += generateSpread(spreadHeight) 
+	return new Particle({
+	x : x,
+	y : y,
+	dx : dx,
+	dy : dy,
+	ax : ax,
+	ay : ay,
+	width : width,
+	height : height
+		
+	})
+}
+
+function generateSpread(spread){
+	return Math.random()*spread -spread/2;
+}
+
+
+function render(){
+	ctx.clearRect(0,0,canvas.width,canvas.height);
+	for(let i=0;i<particles.length;i++){
+		particles[i].update();
+	
+
+	}
+
+requestAnimationFrame(render)
+}
+requestAnimationFrame(render)
